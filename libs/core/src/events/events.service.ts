5c258994efa0684787d7b39962b3d08ec9ed1634
dev-lucasribeiro <lucassribeiro12@gmail.com>
2024-06-21 13:03:11 +0000

    5

--- libs/core/src/events/events.service.ts
+++ libs/core/src/events/events.service.ts
@@ -1,15 +1,98 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateEventDto } from './dto/create-event.dto';
 import { UpdateEventDto } from './dto/update-event.dto';
 import { ReserveSpotDto } from './dto/reserve-spot.dto';
 import { Prisma, SpotStatus, TicketStatus } from '@prisma/client';
 import { PrismaService } from '../prisma/prisma.service';
+import { isValid, parseISO } from 'date-fns';
 
 @Injectable()
 export class EventsService {
   constructor(private prismaService: PrismaService) {}
 
   create(createEventDto: CreateEventDto) {
+    if (!createEventDto.name) {
+      throw new HttpException(
+        'Name is required',
+        HttpStatus.UNPROCESSABLE_ENTITY,
+      );
+    }
+    if (typeof createEventDto.name !== 'string') {
+      throw new HttpException(
+        'Name must be a string',
+        HttpStatus.UNPROCESSABLE_ENTITY,
+      );
+    }
+    if (createEventDto.name.trim().length === 0) {
+      throw new HttpException(
+        'Name cannot be empty or just spaces',
+        HttpStatus.UNPROCESSABLE_ENTITY,
+      );
+    }
+    if (createEventDto.name.length > 255) {
+      throw new HttpException(
+        'Name must be at most 255 characters',
+        HttpStatus.UNPROCESSABLE_ENTITY,
+      );
+    }
+
+    if (!createEventDto.description) {
+      throw new HttpException(
+        'Description is required',
+        HttpStatus.UNPROCESSABLE_ENTITY,
+      );
+    }
+    if (typeof createEventDto.description !== 'string') {
+      throw new HttpException(
+        'Description must be a string',
+        HttpStatus.UNPROCESSABLE_ENTITY,
+      );
+    }
+    if (createEventDto.description.trim().length === 0) {
+      throw new HttpException(
+        'Description cannot be empty or just spaces',
+        HttpStatus.UNPROCESSABLE_ENTITY,
+      );
+    }
+    if (createEventDto.description.length > 255) {
+      throw new HttpException(
+        'Description must be at most 255 characters',
+        HttpStatus.UNPROCESSABLE_ENTITY,
+      );
+    }
+
+    const parsedDate = parseISO(createEventDto.date);
+    if (!createEventDto.date) {
+      throw new HttpException(
+        'Date is required',
+        HttpStatus.UNPROCESSABLE_ENTITY,
+      );
+    }
+    if (!isValid(parsedDate)) {
+      throw new HttpException(
+        'Date must be a valid ISO8601 date',
+        HttpStatus.UNPROCESSABLE_ENTITY,
+      );
+    }
+
+    if (createEventDto.price === undefined) {
+      throw new HttpException(
+        'Price is required',
+        HttpStatus.UNPROCESSABLE_ENTITY,
+      );
+    }
+    if (typeof createEventDto.price !== 'number') {
+      throw new HttpException(
+        'Price must be a number',
+        HttpStatus.UNPROCESSABLE_ENTITY,
+      );
+    }
+    if (createEventDto.price < 0) {
+      throw new HttpException(
+        'Price must be a non-negative number',
+        HttpStatus.UNPROCESSABLE_ENTITY,
+      );
+    }
     return this.prismaService.event.create({
       data: {
         ...createEventDto,
